0c351c80445097bf7016ef3f8f43fe69436717e4
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2019-11-09 09:34:53 +0200

    fix include

--- github-top.user.js
+++ github-top.user.js
@@ -4,7 +4,7 @@
 // @version      0.0.1
 // @description  See top-rated comments in the issue
 // @author       0xC0FFEEC0DE
-// @include      https://github.com/*/*/issues
+// @include      /^https://github.com/(.*)/(.*)/issues/(.*)/
 // @license      MIT
 // ==/UserScript==
 
